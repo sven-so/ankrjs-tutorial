ab7bec259df192cf336020b2fd2f43a03a8121e0
sven-so <svenso@web.de>
2022-12-15 18:21:15 +0100

    next step

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import type { NextPage } from 'next';
+import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { useNfts } from '../hooks';
 import { getNfts } from '../utils';
@@ -8,6 +9,8 @@ const Home: NextPage = () => {
   const [walletAddress, setWalletAddress] = useState(
     '0xe4bBCbFf51e61D0D95FcC5016609aC8354B177C4'
   );
+
+  const { nfts, loading, error } = useNfts(walletAddress);
 
   useEffect(() => {
     (async () => {
@@ -18,23 +21,39 @@ const Home: NextPage = () => {
 
   return (
     <div className='p-10 flex flex-col items-center'>
-      <h1 className='text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl md:text-6xl mb-4"'>
-            NFT Viewer
-       </h1>
+      ...
+      <div className='grid grid-cols-4 mt-8 gap-4'>
+        {nfts.map((nft) => {
+          return (
+            <div
+              key={`${nft.contractAddress}/${nft.tokenId}`}
+              className='flex flex-col rounded border p-4'
+            >
+              <img
+                className='w-[200px] h-[200px] rounded shadow'
+                src={nft.imageUrl}
+                alt={nft.name}
+              />
+              <span className='font-bold mt-8'>{nft.name}</span>
+              <span>{nft.collectionName}</span>
+            </div>
+          );
+        })}
 
-      <div className='flex-left flex-col mt-4'>
-        <label className='text-zinc-700 text-2xl font-extrabold' htmlFor='wallet-address'>
-         &nbsp; Wallet address: &nbsp;
-        </label>
-        <input
-          id='wallet-address'
-          type='text'
-          value={walletAddress}
-          onChange={(e) => setWalletAddress(e.target.value)}
-          className='rounded p-3 w-[425px] border'
-          placeholder='Enter a wallet address here to view NFTs'
-        />
+        {error && (
+          <div className='flex flex-col items-center mt-8'>
+            <p className='text-red-700'>
+              Error: {JSON.stringify(error, null, 2)}
+            </p>
+          </div>
+        )}
       </div>
+
+      <footer className='flex flex-col gap-2 mt-16 items-center'>
+        <Link href='https://github.com/theekrystallee/ankrjs-tutorial'>
+          <a className='text-zinc-700 underline'>Source code</a>
+        </Link>
+      </footer>
     </div>
   );
 };
